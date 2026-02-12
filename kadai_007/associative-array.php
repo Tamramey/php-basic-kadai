082cc70f0baedeaeb2a831b96fd58d74ccd293e5
Tamramey <tam-ramey@ezweb.ne.jp>
2026-02-13 02:41:37 +0900

    kadai_007をアップロード。

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTH-8">
+   <title>課題007</title>
+</head>
+
+<body>
+   <p>
+       <?php
+      //  連想配列に値を代入する
+      $personal_data = ['name' => 'onion', 'price' => 200, 'weight' =>150,];
+
+      print_r($personal_data);
+
+      
