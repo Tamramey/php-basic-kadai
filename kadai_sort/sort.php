0ed004672fd321d1817ad1f361cf2a0d5a9c3b3f
Tamramey <tam-ramey@ezweb.ne.jp>
2026-03-07 02:38:28 +0900

    ソート関数書いてみた。

---
+++ kadai_sort/sort.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+
+
+
+        function sort_2way($array, $order) {
+            if ($order) {
+                // 昇順
+                echo '昇順にソート<br>';
+                sort($array); // 引数に直接配列を渡す
+            } 
+            }
+
+            // ソート結果を表示
+            foreach ($array as $num) {
+                echo $num . ' ';
+            }
+            echo '<br>'; // 各ソート毎に改行
+        
+
+        // ソートする配列を宣言
+        $nums = [15, 4, 18, 23, 10];
+
+        // 昇順ソートの出力
+        sort_2way($nums, true);
+
+        // 降順ソートの出力
+        sort_2way($nums, false);
+        ?>
+    </p>
+</body>
+
+</html>
+        ?>
+    </p>
+</body>
+
+</html>
